ac8757670a1eae9d3247d80697f2ba81295fd85f
Ved Gupta <vedguptaup@gmail.com>
2020-07-22 13:58:55 +0530

    Fixed camfeed_inference

--- frontend.py
+++ frontend.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from model import *
+import sys
 
 def files_inference(weights, device='cpu'):
 	pass
@@ -19,48 +20,49 @@ def camfeed_inference(weights, device='cpu'):
 	classifier.eval()
 
 	class_labels = ['Neutral', 'Happy', 'Surprised', 'Sad', 'Angry']
-
+	flag = False
 	try:
 		cap = cv2.VideoCapture(0)
 
 		while True:
-		    # Grab a single frame of video
-		    ret, frame = cap.read()
-		    labels = []
-		    faces = face_classifier.detectMultiScale(frame,1.3,5)
+			# Grab a single frame of video
+			ret, frame = cap.read()
+			labels = []
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			faces = face_classifier.detectMultiScale(gray,1.3,5)
 
-		    for (x, y, w, h) in faces:
-		        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
-		        roi = frame[y:y+h, x:x+w, :]
-		        roi = cv2.resize(roi, (48,48), interpolation=cv2.INTER_AREA)
+			for (x, y, w, h) in faces:
+				cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+				roi = gray[y:y+h, x:x+w]
+				roi = cv2.resize(roi, (48,48), interpolation=cv2.INTER_AREA)
+				roi = cv2.cvtColor(roi, cv2.COLOR_GRAY2BGR)
 
-		        if np.sum([roi]) != 0:
-		            roi = roi.astype('float')/127.5 - 1
-		            roi = np.transpose(roi, (2,0,1))[::-1, ...]
-		            roi = torch.from_numpy(roi.copy()).unsqueeze(0)
-		            roi = roi.type(torch.FloatTensor).to(device)
+				if np.sum([roi]) != 0:
+					roi = roi.astype('float')/127.5 - 1
+					roi = np.transpose(roi, (2,0,1))[::-1, ...]
+					roi = torch.from_numpy(roi.copy()).unsqueeze(0)
+					roi = roi.type(torch.FloatTensor).to(device)
+					with torch.no_grad():
+						pred = classifier(roi).squeeze()
+					_, ind = torch.max(pred, dim=0)
+					label = class_labels[ind.item()]
+					label_position = (x,y)
+					cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 3)
+				else:
+					cv2.putText(frame, 'No Face Found', (20,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 3)
 
-		        # make a prediction on the ROI, then lookup the class
+			cv2.imshow('Fast Face Expression', frame)
 
-		            pred = classifier(roi).squeeze()
-		            _, ind = torch.max(pred, dim=0)
-		            label = class_labels[ind.item()]
-		            label_position = (x,y)
-		            cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 3)
-		        else:
-		            cv2.putText(frame, 'No Face Found', (20,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 3)
+			if cv2.waitKey(1) == 27: # & 0xFF == ord('q'):
+				flag = True
+				break
 
-		    cv2.imshow('Fast Face Expression', frame)
-
-		    if cv2.waitKey(1) & 0xFF == ord('q'):
-		        break
+		cap.release()
+		cv2.destroyAllWindows()
 
 	except:
 		print("Unexpected error!\n")
-		cap.release()
-		cv2.destroyAllWindows()
-		raise
-
-	cap.release()
-	cv2.destroyAllWindows()
-	
+		if not flag:
+			cap.release()
+			cv2.destroyAllWindows()
+			raise
